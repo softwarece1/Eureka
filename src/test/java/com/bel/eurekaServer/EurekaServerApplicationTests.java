c3b0ec9f67728436fdecd582208be30854913723
softwarece1 <79446145+softwarece1@users.noreply.github.com>
2021-02-24 11:27:47 +0530

    Update EurekaServerApplicationTests.java

--- src/test/java/com/bel/eurekaServer/EurekaServerApplicationTests.java
+++ src/test/java/com/bel/eurekaServer/EurekaServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.bel.eurekaServer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringBootConfiguration;
 
 @SpringBootTest
+@SpringBootConfiguration
 class EurekaServerApplicationTests {
 
 	@Test
